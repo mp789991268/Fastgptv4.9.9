8a3d5708eaf2423cbc3df6726040695ef0ab03ea
Mox莫溪 <1454382661@qq.com>
2025-05-20 21:16:37 +0800

    modified:   .dockerignore 	modified:   projects/app/src/components/core/chat/ChatContainer/ChatBox/components/WelcomeBox.tsx

--- projects/app/src/components/core/chat/ChatContainer/ChatBox/components/WelcomeBox.tsx
+++ projects/app/src/components/core/chat/ChatContainer/ChatBox/components/WelcomeBox.tsx
@@ -8,7 +8,22 @@ import { ChatItemContext } from '@/web/core/chat/context/chatItemContext';
 
 const WelcomeBox = ({ welcomeText }: { welcomeText: string }) => {
   const appAvatar = useContextSelector(ChatItemContext, (v) => v.chatBoxData?.app?.avatar);
+  // 使用正则表达式提取需要弹性布局的数据
+  const result = welcomeText.split('flex--');
 
+  let topTxt = '';
+  let welcomeTextFlex: any[] = [];
+  let bottomTxt = '';
+  // 输出结果
+  if (result.length > 0) {
+    topTxt = result[0];
+  }
+  if (result.length > 2) {
+    welcomeTextFlex = result[1].split('flex-');
+  }
+  if (result.length > 2) {
+    bottomTxt = result[2];
+  }
   return (
     <Box py={3}>
       {/* avatar */}
@@ -22,7 +37,31 @@ const WelcomeBox = ({ welcomeText }: { welcomeText: string }) => {
           bg={'white'}
           boxShadow={'0 0 8px rgba(0,0,0,0.15)'}
         >
-          <Markdown source={`~~~guide \n${welcomeText}`} forbidZhFormat />
+          <Markdown source={`~~~guide \n${topTxt}`} forbidZhFormat />
+          <Box display="flex" gap={'6px'} justifyContent={'space-between'} padding={'6px 0'}>
+            {welcomeTextFlex.map((item, index) => {
+              return (
+                <Box
+                  width={'33%'}
+                  key={index}
+                  backgroundColor={
+                    index == 0
+                      ? 'rgb(255 250 225)'
+                      : index == 1
+                        ? 'rgb(242 238 242)'
+                        : 'rgba(45, 134, 235, 0.05)'
+                  }
+                  padding={'10px'}
+                  borderRadius={'6px'}
+                >
+                  <Markdown source={`~~~guide \n${item}`} forbidZhFormat />
+                </Box>
+              );
+            })}
+          </Box>
+          <Box display={bottomTxt ? 'block' : 'none'}>
+            <Markdown source={`~~~guide \n${bottomTxt}`} forbidZhFormat />
+          </Box>
         </Card>
       </Box>
     </Box>
